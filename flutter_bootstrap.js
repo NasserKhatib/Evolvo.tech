fdf165dbf1fdc5f94d34b629d0be493f14acb693
NasserKhatib <nasserkhatib@users.noreply.github.com>
2026-03-23 01:15:16 -0700

    Fix canvasKitBaseUrl patch (literal newlines)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -35,5 +35,9 @@ if (!window._flutter) {
 }
 _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae12","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"},{}]};
 
-_flutter.loader.load({\n  config: {\n    canvasKitBaseUrl: "/canvaskit/"\n  }\n});
+_flutter.loader.load({
+  config: {
+    canvasKitBaseUrl: "/canvaskit/"
+  }
+});
 
