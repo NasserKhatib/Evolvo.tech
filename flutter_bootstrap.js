e1da5cca97c88828efca9db1e7fe1b4e4a4d8f93
NasserKhatib <nasserkhatib@users.noreply.github.com>
2026-03-23 01:22:52 -0700

    Fix canvasKitBaseUrl path to /Evolvo.tech/canvaskit/

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,7 +37,7 @@ _flutter.buildConfig = {"engineRevision":"052f31d115eceda8cbff1b3481fcde4330c4ae
 
 _flutter.loader.load({
   config: {
-    canvasKitBaseUrl: "/canvaskit/"
+    canvasKitBaseUrl: "/Evolvo.tech/canvaskit/"
   }
 });
 
